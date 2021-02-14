568700b91732aadc83a6bbc4ec8187faafabae4d
admin <admin@DESKTOP-R0RTESC>
2021-02-14 10:46:27 +0530

    First commit

---
+++ KarthikApp/src/main/java/com/app/KarthikAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarthikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarthikAppApplication.class, args);
+	}
+
+}
